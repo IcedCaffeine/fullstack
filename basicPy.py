e314a7caceb93b5291530785cbeab78da9fef91b
IcedCaffeine <fahad7923@gmail.com>
2024-08-30 12:16:56 -0600

    random message added

--- basicPy.py
+++ basicPy.py
@@ -1,4 +1,10 @@
+import random
+
+
 print("hello world")
-receivedText = input("What she said: ")
+receivedText = input("What she messaged you: ")
 print(receivedText)
-print("end")
+messageArray = ["you are cute", "Hey whatre you up to", "Lets grab a drink"]
+randomMessage = messageArray[random.randint(0, 2)]
+print("Send her this: ")
+print(randomMessage)
