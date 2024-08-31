4ddb5f29c3ddbcc222a063aaa0d574a4c49c7162
IcedCaffeine <fahad7923@gmail.com>
2024-08-31 02:29:38 -0600

    name, loop, goodbye

--- basicPy.py
+++ basicPy.py
@@ -1,10 +1,17 @@
 import random
 
 
-print("hello world")
-receivedText = input("What she messaged you: ")
-print(receivedText)
-messageArray = ["you are cute", "Hey whatre you up to", "Lets grab a drink"]
-randomMessage = messageArray[random.randint(0, 2)]
-print("Send her this: ")
-print(randomMessage)
+print("Welcome to Sigma Rizzler")
+print("Ready to start?")
+receivedText = input("yes to start/exit to quit: ")
+while( receivedText != "exit"):
+    receivedText = input("What did she message you: ")
+    if receivedText == "exit":
+        break
+    print(receivedText)
+    messageArray = ["you are cute", "Hey whatre you up to", "Lets grab a drink", "How do you like your coffee?"]
+    randomMessage = messageArray[random.randint(0, 3)]
+    print("Send her this: ")
+    print(randomMessage)
+print("Goodbye!")
+    
